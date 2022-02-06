29fa4807f83feb32c9cebfeb051eefdb861f10b6
Dmytro Ohorodnik <me@dmytroohorodnik.com>
2022-02-06 16:36:11 +0100

    Add Solidity tree-sitter parser

--- config/nvim/lua/settings/nvim-treesitter.lua
+++ config/nvim/lua/settings/nvim-treesitter.lua
@@ -6,3 +6,12 @@ require('nvim-treesitter.configs').setup {
         additional_vim_regex_highlighting = false
     }
 }
+
+require"nvim-treesitter.parsers".get_parser_configs().solidity = {
+    install_info = {
+        url = "https://github.com/JoranHonig/tree-sitter-solidity",
+        files = {"src/parser.c"},
+        requires_generate_from_grammar = true
+    },
+    filetype = 'solidity'
+}
