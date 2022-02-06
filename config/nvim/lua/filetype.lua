db8ef92893920c795ac14b4b2cb04eab66f75bca
Dmytro Ohorodnik <me@dmytroohorodnik.com>
2022-02-06 16:33:10 +0100

    Add solidity support (very limited still)

---
+++ config/nvim/lua/filetype.lua
@@ -0,0 +1,3 @@
+vim.cmd [[
+  au BufNewFile,BufRead *.sol setfiletype solidity
+]]
---
+++ config/nvim/lua/settings/nvim-lspconfig/solc.lua
@@ -0,0 +1,7 @@
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+require('lspconfig').solc.setup {
+  capabilities = capabilities
+}
