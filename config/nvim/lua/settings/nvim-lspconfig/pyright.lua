82c61aa8471be8379b2ade1602423fc9a3ab7eec
Dmytro Ohorodnik <dmytroohorodnik@protonmail.com>
2022-11-07 12:34:12 -0800

    fix: use 'cmp_nvim_lsp.default_capabilities()' instead of deprecated 'cmp_nvim_lsp.update_capabilities()'

--- config/nvim/lua/settings/nvim-lspconfig/pyright.lua
+++ config/nvim/lua/settings/nvim-lspconfig/pyright.lua
@@ -1,8 +1,7 @@
 vim.g["python3_host_prog"] = "/usr/bin/python3"
 
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
--- config/nvim/lua/settings/nvim-lspconfig/sumneko_lua.lua
+++ config/nvim/lua/settings/nvim-lspconfig/sumneko_lua.lua
@@ -1,6 +1,5 @@
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
