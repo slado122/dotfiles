34a52fadfdb93e2a76309f4fad6a61a165bd8330
Dmytro Ohorodnik <dmytroohorodnik@protonmail.com>
2023-05-04 21:46:02 -0700

    feat: adjust cargo test runner options

--- config/nvim/after/plugin/vimtest.lua
+++ config/nvim/after/plugin/vimtest.lua
@@ -2,5 +2,6 @@ vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>")
 vim.cmd [[
     let test#strategy = "neovim"
     let test#python#djangotest#options = "--keepdb"
+    let test#rust#cargotest#options = "-- --include-ignored --nocapture"
 ]]
 
